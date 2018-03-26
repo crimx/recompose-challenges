960e154972375fd0ad1b3b79c8c8ad1101676cd7
CRIMX <straybugs@gmail.com>
2018-03-26 23:13:55 +0800

    feat(challenge): add challenge07

---
+++ src/challenges/Challenge07.tsx
@@ -0,0 +1,56 @@
+/**
+ * Implement `renderNothing`, a HOC that always renders `null`
+ * https://github.com/acdlite/recompose/blob/master/docs/API.md#rendernothing
+ */
+
+import React, { Component, ComponentClass, SFC } from 'react'
+import { compose } from './Challenge03'
+import { withStateHandlers } from './Challenge04'
+import { lifecycle } from './Challenge05'
+import { bracnch } from './Challenge06'
+
+/**
+ * Implementation
+ */
+
+class Nothing extends Component<null> {
+  render () { return null }
+}
+
+export const renderNothing = (Comp: any): ComponentClass<null> => Nothing
+
+/**
+ * Usage
+ */
+
+interface Data {
+  msg: string
+}
+
+const DisplayData: SFC<{ data: Data }> = ({ data }) => <h1>{data.msg}</h1>
+
+const enhance = compose(
+  withStateHandlers(
+    { data: null },
+    {
+      updateData: () => newData => ({ data: newData })
+    }
+  ),
+  lifecycle<{ updateData: any }, { data: Data}>({
+    componentDidMount () {
+      fetchData().then(this.props.updateData).catch(console.error)
+    }
+  }),
+  bracnch<{ data: Data }>(
+    props => !props.data,
+    renderNothing
+  )
+)
+
+export default enhance<any>(DisplayData)
+
+function fetchData (): Promise<Data> {
+  return new Promise(resolve => {
+    setTimeout(() => resolve({ msg: 'New data!' }), 2000)
+  })
+}
